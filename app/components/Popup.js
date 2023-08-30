34467384474812132d593d591ce810c163830723
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2023-08-30 22:37:39 +0000

    Optimised for mobile Removed redundant code

--- app/components/Popup.js
+++ app/components/Popup.js
@@ -5,20 +5,17 @@ import { useEffect, useState } from "react"
 export function changePopup(changeValue)
 {
     const p = document.getElementById('popup')
-    p.innerHTML = `<p class="text-blue text-[18px] font-semibold bg-white border-2 border-lightBlue rounded-full shadow-md px-8 py-4">${changeValue}</p>`
+    p.innerHTML = `<p class="text-blue text-[15px] md:text-[18px] font-medium md:font-semibold bg-white border-2 border-lightBlue rounded-full shadow-md px-4 md:px-8 py-2 md:py-4">${changeValue}</p>`
     p.style.opacity = '1'
-    setTimeout(()=>{p.style.opacity='0'}, 5000);
+    setTimeout(()=>{p.style.opacity='0'}, 3500);
 }
 
 
-export default function Popup({ statefulVariable})
+export default function Popup()
 {
-    const [ popupText, changePopup ] = useState('')
-    
-
     return (
         <>
-            <div id="popup" className="w-[100vw] flex justify-center absolute bottom-5">
+            <div id="popup" className="w-[100vw] flex justify-center absolute bottom-12 md:bottom-5">
             </div>
         </>
     )
