995a023fa07dcea996e2a1f3a1d2b6a17b34bd5f
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2023-08-31 22:44:30 +0000

    Sorted out date issue

--- app/utils/custom-date.js
+++ app/utils/custom-date.js
@@ -2,7 +2,9 @@
 
 export function currDate()
 {
-    const today = new Date()
-    const dateString = today.getFullYear().toString() + ((today.getMonth() < 10) ? "0" + today.getMonth().toString(10): today.getMonth().toString(10))  + ((today.getDay() < 10) ? "0" + today.getDay().toString(10): today.getDay().toString(10)) + ((today.getHours() < 10) ? "0" + today.getHours().toString(10): today.getHours().toString(10))
+    var today = new Date()
+    //console.log(today.getMonth(), today.getDate()+1, today.getHours())
+    const dateString = today.getFullYear().toString() + (((today.getMonth() + 1) < 10) ? `0${today.getMonth() + 1}`: (today.getMonth() + 1))  + ((today.getDate() < 10) ? `0${today.getDate()}`: today.getDate().toString()) + ((today.getHours() < 10) ? "0" + today.getHours().toString(): today.getHours().toString())
+    console.log(dateString)
     return dateString
 }
