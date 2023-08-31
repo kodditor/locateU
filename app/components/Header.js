86c9e896280b84ba85d1bf2a97b1b5bbe523a21a
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2023-08-31 22:46:02 +0000

    Added option for searchbar

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,7 +1,9 @@
+import SearchBarMob from "./searchBarMob";
 
 
-export default function Header({onlyLogo, centered})
+export default function Header({onlyLogo, centered, searchBar})
 {
+
     return(
         <>
         <div className={`flex flex-row ${(centered)? "justify-center": "justify-between"} ml-5 mr-5 md:ml-10 md:mr-10 pt-4`}>
@@ -9,13 +11,16 @@ export default function Header({onlyLogo, centered})
                 <a href="/"><img src={'/img/logo.png'} /></a>
             </div>
 
-           {!onlyLogo && <div className="hidden flex flex-row justify-between gap-8 items-center">
-                <div className="hidden md:block">
+           {!onlyLogo && <div className="flex flex-row justify-between gap-8 items-center">
+                {!searchBar && <>
+                <div className="hidden">
                     <a href="/request-location" className="blue hover:font-semibold text-center text-blue duration-150">Request a new location</a>
                 </div>
-                <div>
+                <div className="hidden">
                     <a href="/contact"><button className="text-white bg-blue px-3 py-2 md:px-5 md:py-3 border-2 font-semibold rounded-full hover:bg-transparent hover:text-blue duration-150">Contact</button></a>
                 </div>
+                </>}
+                {searchBar && <SearchBarMob />}
             </div>
             }
         </div>
