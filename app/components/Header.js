2e58795657f4921cb9570685c3883f38a1ee77ba
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2023-09-18 12:48:29 +0000

    Made the header absolute on mobile devices

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,17 +1,17 @@
 import SearchBarMob from "./searchBarMob";
 
 
-export default function Header({onlyLogo, centered, searchBar})
-{
+export default function Header({onlyLogo, centered, searchBar, absolute})
+{ 
 
     return(
         <>
-        <div className={`flex flex-row ${(centered)? "justify-center": "justify-between"} ml-5 mr-5 md:ml-10 md:mr-10 pt-4`}>
-            <div className="w-32 md:w-48 flex items-center">
+        <div className={` w-[calc(100%-2.5rem)] flex flex-row ${(centered)? "justify-center": "justify-between"} ${absolute? "absolute md:static bg-white md:bg-transparent rounded-2xl md:rounded-none shadow-md md:shadow-none p-2 mt-4 md:mt-0": ""}  ml-5 mr-5 md:ml-10 md:mr-10 md:p-0 md:pt-4`}>
+            <div className={`w-32 md:w-48 flex items-center`}>
                 <a href="/"><img src={'/img/logo.png'} /></a>
             </div>
 
-           {!onlyLogo && <div className="flex flex-row justify-between gap-8 items-center">
+           {!onlyLogo && <div className="flex flex-row md:justify-between md:gap-8 items-center">
                 {!searchBar && <>
                 <div className="hidden">
                     <a href="/request-location" className="blue hover:font-semibold text-center text-blue duration-150">Request a new location</a>
